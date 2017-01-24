a22f3c9d7714defacc056b6410df46ce2382d170
deefourple <davedehghani@gmail.com>
2017-01-24 16:53:39 +0000

    Implemented a visual character coder in the text box that alerts user if too many characters are used

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#textField').on("keyup", function(){
+    let screenTotal = 140;
+    //select the textarea and find its length
+    let charCount = $(this).val().length;
+    //set counter to subtract from the length of the textarea
+    let currentCount = screenTotal - charCount;
+
+    //display the currentCount variable on the screen
+    $('.counter').text(currentCount);
+    if (currentCount < 0) {
+      $('.counter').text(currentCount).css("color", "#ff0000").css("font-size", "1.15em");
+    }
+    else {
+      $('.counter').text(currentCount).css("color", "#4adb1a").css("font-size", "1em");
+    }
+  });
+});
