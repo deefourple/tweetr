9b09eba6441460f35c5215b0555b26ee96478a9b
deefourple <davedehghani@gmail.com>
2017-01-26 03:11:40 +0000

    Form validation for possible input errors

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,47 +2,30 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+   */
 
- var hover = function() {
-    $('.tweet-container article').on("mouseenter", function(event) {
-    let tweetArticle = $(event.target);
-    let buttons = tweetArticle.find('.hover-buttons');
-    let profilePic = tweetArticle.find('.pic');
-    let fullName = tweetArticle.find('.fullName');
-    let userName = tweetArticle.find('.userName');
-    buttons.css("display", "inline")
-    profilePic.css("opacity", "1");
-    fullName.css("color", "#244751");
-    userName.css("color", "#bbb")
-  });
-  $('.tweet-container article').on("mouseleave", function(event) {
-    let tweetArticle = $(event.target);
-    let buttons = tweetArticle.find('.hover-buttons');
-    let profilePic = tweetArticle.find('.pic');
-    let fullName = tweetArticle.find('.fullName');
-    let userName = tweetArticle.find('.userName');
-    buttons.css("display", "none");
-    profilePic.css("opacity", "0.5");
-    fullName.css("color", "rgba(36, 71, 81, 0.5)");
-    userName.css("color", "rgba(186, 186, 186, 0.5)")
-  })
-}
 $(document).ready(function(){
   fetchTweets()
-
-//create a hidden flash message that fades in when a condition is met
+  $('.compose-button').on('click', function(event) {
+    event.preventDefault()
+    var composeBox = $('.new-tweet');
+    if (composeBox.css("display") === "none"){
+    composeBox.slideDown(900);
+    $('#textField').focus()
+    } else {
+      composeBox.slideUp(900);
+    }
+  });
   let form = $('.container form');
   form.on('submit', function(event) {
-     if ($('#textField').val().length < 1){
+     if ($('#textField').val().length === 0){
         event.preventDefault();
         $('.flash').fadeIn(2000).css("display", "inline").fadeOut(2000);
-     } else if ($('#textField').val().length >= 140) {
+     } else if ($('#textField').val().length > 140) {
         event.preventDefault();
         $('.flash2').fadeIn(2000).css("display", "inline").fadeOut(2000);
      } else {
     event.preventDefault();
-    // console.log($('#textField').val().length)
     $('.tweet-container').empty();
     $.ajax('/tweets', {method: "post", data: $('#textField')})
     .then((result) => {
@@ -51,6 +34,7 @@ $(document).ready(function(){
     })
     .fail((error) => console.error(error))
     }
+    $('#textField').val('')
   })
 });
 
@@ -64,18 +48,35 @@ function renderTweets(tweets) {
    let newTweet = createTweetElement(tweet)
    $('.tweet-container').prepend(newTweet);
   })
-  hover()
 }
 
-//create conditional statement if(string is empty ""), else if(too many characters) else (run tweet)
+function timeConverter(postTime) {
+  let present = Date.now();
+  let days = Math.floor((present - postTime) / 86400000);
+  if (days > 1) {
+    return `${days} days ago`;
+  } else if (days === 1) {
+    return `${days} day ago`;
+  } else {
+    let hours = Math.floor(days / 24);
+    if (hours > 1) {
+      return `${hours} hours ago`;
+    } else if (hours === 1) {
+      return `${hours} hour ago`;
+    } else {
+      return `less than 1 hour ago`;
+    }
+  }
+}
+
 function createTweetElement(twts){
   let userName = twts.user.name;
   let avatarSmall = twts.user.avatars.small;
   let handle = twts.user.handle;
   let content = twts.content.text;
-  let created = twts.created_at;
+  let created = timeConverter(twts.created_at);
 
-  let newTweet = $(` <article>
+  let newTweet = $(` <article class="tweet">
                      <header class="tweet-title">
                        <img class="pic" src=${avatarSmall}>
                        <h2 class="fullName"> ${userName}</h2>
